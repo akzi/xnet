75914caaa2b6ba34d92a8bbd5a5799367c269556
akzi <382018309@qq.com>
2017-01-12 17:35:10 +0100

    add codes

--- samples/mt_server/mt_server.cpp
+++ samples/mt_server/mt_server.cpp
@@ -13,37 +13,45 @@ int main()
 	using namespace xnet;
 	proactor_pool pp;
 	std::mutex mtx;
-	std::vector<connection> connections;
+	std::vector<std::pair<connection, std::list<std::string>>> connections;
 	std::list<std::size_t> index_;
 	connections.reserve(10000);
 
 	pp.regist_accept_callback([&](connection &&conn) {
 		std::lock_guard<std::mutex> lg(mtx);
 		std::size_t index;
-		connections.emplace_back(std::move(conn));
+		connections.emplace_back(std::move(conn), std::list<std::string>());
 		index = connections.size() - 1;
 
-		connections[index].regist_recv_callback(
+		connections[index].first.regist_recv_callback(
 			[index,rsp,&connections,&index_](char *data, std::size_t len) {
 			auto id = index;
 			if (len == 0)
 			{
-				connections[id].close();
+				connections[id].first.close();
 				return;
 			}
 			//std::cout << std::this_thread::get_id() << std::endl;
-			connections[id].async_send(rsp, (int)strlen(rsp));
-			connections[id].async_recv_some();
+			if (connections[id].second.empty())
+			{
+				connections[id].first.async_send(rsp, (int)strlen(rsp));
+				connections[id].second.emplace_back(rsp);
+			}
+			connections[id].first.async_recv_some();
 		});
-		connections[index].regist_send_callback(
+		connections[index].first.regist_send_callback(
 			[index, &connections, &index_](std::size_t len) {
 			auto id = index;
 			if (len == 0)
 			{
-				connections[id].close();
+				connections[id].first.close();
 			}
+			connections[id].second.pop_front();
+			if (connections[id].second.empty())
+				return;
+			connections[id].first.async_send(std::move(connections[id].second.front()));
 		});
-		connections[index].async_recv_some();
+		connections[index].first.async_recv_some();
 
 	}).start();
 	pp.bind("0.0.0.0", 9001);
